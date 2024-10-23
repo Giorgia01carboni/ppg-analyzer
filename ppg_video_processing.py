030777e24c64bc2a7408e5c7dd2190121c0cedd9
giorgia01carboni <giorgia.carboni@gmail.com>
2024-10-23 14:01:10 +0200

    Finished code

--- ppg_video_processing.py
+++ ppg_video_processing.py
@@ -1,24 +1,57 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-# heart rate from smartphone
+# Open the video file
 hr_video = cv2.VideoCapture("IMG_6062.MOV")
 
-if (hr_video.isOpened() == False):
-  print("Error opening video file")
+if not hr_video.isOpened():
+    print("Error opening video file")
 
-# store average red intensities for each frame
+# Store average red intensities for each frame
 red_intensities = []
+total_frames = int(hr_video.get(cv2.CAP_PROP_FRAME_COUNT))
+print(f"Total frames in video: {total_frames}")
 
-while (hr_video.isOpened()):
-  ret, frame = hr_video.read()
-  if ret == True:
+# Time vector
+time_vector = []
+# Loop from 0 to T-1. 30 is number of frames per second
+for i in range(total_frames):
+    time_vector.append(i/30)
+
+# Read frames until the end of the video
+while True:
+    ret, frame = hr_video.read()
+    if not ret:  # Break if no frame is returned
+        break
+    # Split the frame into color channels and compute the mean intensity of the red channel
     (b_channel, g_channel, r_channel) = cv2.split(frame)
     red_intensities.append(np.mean(r_channel))
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-  else:
-    break
 
+# Release the video capture object
 hr_video.release()
+
+# Print the total number of red intensities recorded
+print(f"Total red intensities recorded: {len(red_intensities)}")
+
+plt.plot(time_vector, red_intensities)
+plt.show()
+
+heart_rate_dft = np.fft.fft(red_intensities)
+f_axis = np.fft.fftfreq(total_frames, d=1/30)
+magnitude_axis = np.abs(heart_rate_dft)
+omega_axis = np.angle(heart_rate_dft)
+
+plt.plot(f_axis, magnitude_axis)
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Magnitude')
+plt.title('Magnitude Spectrum of Red Intensity Signal')
+plt.show()
+
+plt.plot(omega_axis, magnitude_axis)
+plt.xlabel('Angular Frequency (rad/s)')
+plt.ylabel('Magnitude')
+plt.title('Phase Spectrum of Red Intensity Signal')
+plt.show()
+
+
